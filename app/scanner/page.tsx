9c753eb89dd91472a621cb21056f71784e402cfa
arthurkoblet210 <arthurkoblet210@gmail.com>
2024-04-26 02:45:05 -0400

    change ti

---
+++ app/scanner/page.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import LoginPage from "@/templates/LoginPage";
+
+const Login: NextPage = () => {
+    return <LoginPage />;
+};
+
+export default Login;
+    
